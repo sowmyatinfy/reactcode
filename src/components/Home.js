b7ceb920c5eb299eaa0d191946ed7be55e4b57eb
sowmyatinfy <74291377+sowmyatinfy@users.noreply.github.com>
2021-03-04 17:42:52 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const searchBox = () => {
+    return( <input type='search' className = 'search' placeholder = {props.placeholder} onChange = {} />)
+
+}
+export  default  searchBox;
